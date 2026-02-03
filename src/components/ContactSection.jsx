4fe4cf00d1c6a3c9f10534a1dfdf646628c8ca09
arzamansoori <arzamansoori786@gmail.com>
2026-02-03 12:36:28 +0530

    Make footer responsive

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -37,137 +37,137 @@ ${formData.name}`;
 
   return (
     <div id="contact">
-    <div className="pt-40 mb-20">
+      <div className="pt-24 md:pt-32 mb-20">
 
-      <div className="text-center">
-        <h1 className="pb-2 text-4xl font-semibold text-[#FFEFCD]">Get In Touch</h1>
-        <p className="text-lg text-[#A58E74]">Ready to transform your space? Let's discuss your vision</p>
-      </div>
-
-      <div className="mt-10 flex gap-40 justify-center">
-
-        {/* Contact Information */}
-        <div className="">
-          <h3 className="text-[#FFEFCD] font-semibold text-2xl pb-4">Contact Information</h3>
-
-          <div className="pb-10 flex flex-col gap-4">
-            {/* phone */}
-            <div className="flex items-center gap-3">
-              <div className="text-[#E09132]">
-                <FaPhone />
-              </div>
-              <div>
-                <p className="text-[#FFEFCD]">Phone</p>
-                <p className="text-[#A58E74]">+91-9036925738</p>
-              </div>
-            </div>
-
-            {/* email */}
-            <div className="flex items-center gap-3">
-              <div className="text-[#E09132] text-xl">
-                <MdEmail />
-              </div>
-              <div>
-                <p className="text-[#FFEFCD]">Email</p>
-                <p className="text-[#A58E74]">ardesignbuild03@gmail.com</p>
-              </div>
-            </div>
-
-            {/* address */}
-            <div className="flex items-center gap-3">
-              <div className="text-[#E09132] text-2xl">
-                <MdLocationOn />
-              </div>
-              <div>
-                <p className="text-[#FFEFCD]">Address</p>
-                <p className="text-[#A58E74]">FM Ali's, Tolli Chowki,<br/> Hyderabad</p>
-              </div>
-            </div>
-
-          </div>
-
-          <div>
-            <p className="text-[#FFEFCD] font-semibold text-lg pb-4">Follow us</p>
-            <div className="flex gap-3">
-              <a
-                href= "https://www.instagram.com/ardesign_build03/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="p-3 rounded-full bg-[#E09132] cursor-pointer inline-flex items-center justify-center"
-              >
-                <FaInstagram />
-              </a>
-
-              <a
-                href="https://www.linkedin.com/in/arshi-farheen-mansoori-347486109/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="p-3 rounded-full bg-[#E09132] cursor-pointer inline-flex items-center justify-center"
-              >
-                <FaLinkedin />
-              </a>
-            </div>
-          </div>
+        <div className="text-center">
+          <h1 className="pb-2 text-4xl font-semibold text-[#FFEFCD]">Get In Touch</h1>
+          <p className="text-lg text-[#A58E74]">Ready to transform your space? Let's discuss your vision</p>
         </div>
 
-        {/* Message form */}
-        <div className="border border-[#5b5e45] rounded-xl p-6 w-96">
-          <h3 className="text-[#FFEFCD] text-2xl font-semibold pb-6">Send a Message</h3>
-          <form onSubmit={handleSubmit} className="flex flex-col gap-6">
-            <div>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Name"
-                value={formData.name}
-                onChange={handleChange}
-                className="border border-[#5b5e45] px-3 py-2 rounded-lg text-[#FFEFCD] w-full 
-                  focus:outline-none focus:ring-0 focus:border-[#FFEFCD]"
-                required
-              />
-            </div>
+        <div className="mt-10 flex lg:gap-40 gap-10 justify-center flex-wrap-reverse mx-4">
 
-             <div>
-              <input
-                type="text"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Email"
-                className="border border-[#5b5e45] px-3 py-2 rounded-lg text-[#FFEFCD] w-full 
-                  focus:outline-none focus:ring-0 focus:border-[#FFEFCD]"
-                required
-              />
+          {/* Contact Information */}
+          <div className="">
+            <h3 className="text-[#FFEFCD] font-semibold text-2xl pb-4">Contact Information</h3>
+
+            <div className="pb-10 flex flex-col gap-4">
+              {/* phone */}
+              <div className="flex items-center gap-3">
+                <div className="text-[#E09132]">
+                  <FaPhone />
+                </div>
+                <div>
+                  <p className="text-[#FFEFCD]">Phone</p>
+                  <p className="text-[#A58E74]">+91-9036925738</p>
+                </div>
+              </div>
+
+              {/* email */}
+              <div className="flex items-center gap-3">
+                <div className="text-[#E09132] text-xl">
+                  <MdEmail />
+                </div>
+                <div>
+                  <p className="text-[#FFEFCD]">Email</p>
+                  <p className="text-[#A58E74]">ardesignbuild03@gmail.com</p>
+                </div>
+              </div>
+
+              {/* address */}
+              <div className="flex items-center gap-3">
+                <div className="text-[#E09132] text-2xl">
+                  <MdLocationOn />
+                </div>
+                <div>
+                  <p className="text-[#FFEFCD]">Address</p>
+                  <p className="text-[#A58E74]">FM Ali's, Tolli Chowki,<br/> Hyderabad</p>
+                </div>
+              </div>
+
             </div>
 
             <div>
-              <input
-                type="text"
-                id="projectDetails"
-                name="projectDetails"
-                value={formData.projectDetails}
-                onChange={handleChange}
-                placeholder="Tell us about your project..."
-                className="border border-[#5b5e45] px-3 py-2 rounded-lg text-[#FFEFCD] w-full 
-                  focus:outline-none focus:ring-0 focus:border-[#FFEFCD]"
-                required
-              />
-            </div>
+              <p className="text-[#FFEFCD] font-semibold text-lg pb-4">Follow us</p>
+              <div className="flex gap-3">
+                <a
+                  href= "https://www.instagram.com/ardesign_build03/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="p-3 rounded-full bg-[#E09132] cursor-pointer inline-flex items-center justify-center"
+                >
+                  <FaInstagram />
+                </a>
 
-            <button 
-              type="submit"
-              className="px-2 py-2 bg-[#E09132] text-[#424530] font-medium rounded-lg cursor-pointer">
-                Send Message
-            </button>
-          </form>
+                <a
+                  href="https://www.linkedin.com/in/arshi-farheen-mansoori-347486109/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="p-3 rounded-full bg-[#E09132] cursor-pointer inline-flex items-center justify-center"
+                >
+                  <FaLinkedin />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Message form */}
+          <div className="border border-[#5b5e45] rounded-xl p-6 w-96">
+            <h3 className="text-[#FFEFCD] text-2xl font-semibold pb-6">Send a Message</h3>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+              <div>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="border border-[#5b5e45] px-3 py-2 rounded-lg text-[#FFEFCD] w-full 
+                    focus:outline-none focus:ring-0 focus:border-[#FFEFCD]"
+                  required
+                />
+              </div>
+
+              <div>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Email"
+                  className="border border-[#5b5e45] px-3 py-2 rounded-lg text-[#FFEFCD] w-full 
+                    focus:outline-none focus:ring-0 focus:border-[#FFEFCD]"
+                  required
+                />
+              </div>
+
+              <div>
+                <input
+                  type="text"
+                  id="projectDetails"
+                  name="projectDetails"
+                  value={formData.projectDetails}
+                  onChange={handleChange}
+                  placeholder="Tell us about your project..."
+                  className="border border-[#5b5e45] px-3 py-2 rounded-lg text-[#FFEFCD] w-full 
+                    focus:outline-none focus:ring-0 focus:border-[#FFEFCD]"
+                  required
+                />
+              </div>
+
+              <button 
+                type="submit"
+                className="px-2 py-2 bg-[#E09132] text-[#424530] font-medium rounded-lg cursor-pointer">
+                  Send Message
+              </button>
+            </form>
+          </div>
         </div>
+
       </div>
 
-    </div>
-
-    <div className="border border-[#5b5e45]"></div>
+      <div className="border border-[#5b5e45]"></div>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,55 +3,57 @@ import { AiOutlineCopyright } from "react-icons/ai";
 const Footer = () => {
   return (
     <div id="Footer">
-    <div className="pt-20 pb-10 flex gap-28 justify-center flex-wrap">
+    <div className="pt-20 pb-10 flex lg:flex-row flex-col items-center lg:gap-20 gap-10 lg:justify-center flex-wrap mx-5">
 
       <div>
         <h3 className="text-[#FFEFCD] font-semibold text-xl pb-2">ARDesign&Build</h3>
-        <p className="text-[#A58E74]">Creating beautiful spaces that
+        <p className="text-[#A58E74] text-sm">Creating beautiful spaces that
           <br />inspire and transform lives
           <br />through thoughtful design.
         </p>
       </div>
 
-      <div>
-        <h3 className="text-[#FFEFCD] font-semibold text-md pb-2">Services</h3>
-        <ul className="text-[#A58E74] flex flex-col gap-2">
-          <li>Residential Design</li>
-          <li>Commercial Spaces</li>
-          <li>Consultation</li>
-          <li>Space Planning</li>
-        </ul>
-      </div>
+      <div className="flex md:gap-20 gap-10 md:flex-row flex-col">
+        <div>
+          <h3 className="text-[#FFEFCD] font-semibold text-md pb-2">Services</h3>
+          <ul className="text-[#A58E74] flex flex-col gap-2 text-sm">
+            <li>Residential Design</li>
+            <li>Commercial Spaces</li>
+            <li>Consultation</li>
+            <li>Space Planning</li>
+          </ul>
+        </div>
 
-      <div>
-        <h3 className="text-[#FFEFCD] font-semibold text-md pb-2">Quick Links</h3>
-        <ul className="text-[#A58E74] flex flex-col gap-2">
-          <a href="#portfolio" className="hover:text-[#a18955]">Portfolio</a>
-          <a href="#about" className="hover:text-[#a18955]">About</a>
-          <a href="#reviews" className="hover:text-[#a18955]">Reviews</a>
-          <a href="#contact" className="hover:text-[#a18955]">Contact</a>
-        </ul>
-      </div>
+        <div>
+          <h3 className="text-[#FFEFCD] font-semibold text-md pb-2">Quick Links</h3>
+          <ul className="text-[#A58E74] flex flex-col gap-2 text-sm">
+            <a href="#portfolio" className="hover:text-[#a18955]">Portfolio</a>
+            <a href="#about" className="hover:text-[#a18955]">About</a>
+            <a href="#reviews" className="hover:text-[#a18955]">Reviews</a>
+            <a href="#contact" className="hover:text-[#a18955]">Contact</a>
+          </ul>
+        </div>
 
-      <div>
-        <h3 className="text-[#FFEFCD] font-semibold text-md pb-2">Contact</h3>
-        <ul className="text-[#A58E74] flex flex-col gap-2">
-          <li>+91-9036925738</li>
-          <li>ardesignbuild03@gmail.com</li>
-          <li>FM Ali's, Tolli Chowki,
-            <br />Hyderabad
-          </li>
-        </ul>
+        <div>
+          <h3 className="text-[#FFEFCD] font-semibold text-md pb-2">Contact</h3>
+          <ul className="text-[#A58E74] flex flex-col gap-2 text-sm">
+            <li>+91-9036925738</li>
+            <li>ardesignbuild03@gmail.com</li>
+            <li>FM Ali's, Tolli Chowki,
+              <br />Hyderabad
+            </li>
+          </ul>
+        </div>
       </div>
 
     </div>
 
-    <div className="border border-[#5b5e45] mx-50 mb-4"></div>
+    <div className="border-t border-[#5b5e45] my-4 mx-10"></div>
 
     {/* copyright */}
     <div className="flex items-center gap-1 justify-center text-[#A58E74] pb-10 pt-5">
       <span><AiOutlineCopyright /></span>
-      <p>2026 ARDesign&Build. All rights reserved.</p>
+      <p className="text-sm flex flex-wrap">2026 ARDesign&Build. All rights reserved.</p>
     </div>
     </div>
   )
