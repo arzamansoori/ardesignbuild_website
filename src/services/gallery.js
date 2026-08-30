c4bbfdf3e87421527fe7ab3329f8d8e4aa695a2f
shivambl <shivam.bansal@live.com>
2026-08-31 00:15:29 +0530

    Add delivered media to gallery

--- src/services/gallery.js
+++ src/services/gallery.js
@@ -15,6 +15,34 @@ const projectImages = projects.flatMap((project) =>
   }))
 );
 
+const deliveredImages = projects.flatMap((project) =>
+  (project.delivered?.images ?? []).map((image) => ({
+    img: image.img,
+    width: image.width,
+    height: image.height,
+    caption: image.caption,
+  }))
+);
+
+const deliveredVideos = projects.flatMap((project) =>
+  project.delivered?.video
+    ? [
+        {
+          video: project.delivered.video.src,
+          width: project.delivered.video.width,
+          height: project.delivered.video.height,
+          caption: project.delivered.video.caption,
+        },
+      ]
+    : []
+);
+
 export const galleryImages = [...portfolioImages, ...projectImages];
+
+export const galleryMedia = [
+  ...galleryImages,
+  ...deliveredImages,
+  ...deliveredVideos,
+];
 
 export default galleryImages;
